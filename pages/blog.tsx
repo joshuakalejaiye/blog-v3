e8dd04a2f8cd6fbda1e5842f3e33826d0cb08ac4
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2023-02-22 15:01:32 +0000

    blog page desktop mostly done

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,8 +3,16 @@ import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/pages/Blog.module.scss";
 
-export default function Blog() {
-  const pageTitle = "Blog";
+const data = {
+  author: "ME",
+  lastUpdated: "12:18 BST",
+  timeToRead: "5 min read",
+};
+
+export default function BlogPage() {
+  const pageTitle = "BlogPage";
+
+  const { author, lastUpdated, timeToRead } = data;
 
   return (
     <Layout flex title={pageTitle}>
@@ -14,7 +22,7 @@ export default function Blog() {
             animate={{ opacity: 1 }}
             initial={{ opacity: 0 }}
             transition={{
-              delay: 0.5,
+              delay: 0.8,
               opacity: { duration: 0.2 },
             }}
             className={`${styles.title}`}
@@ -22,15 +30,36 @@ export default function Blog() {
             Microsoft President Is Carrying That Giant Sony Call of Duty Deal In
             Pocket, Weirdly
           </motion.h1>
-          <div className={styles.imageContainer}>
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{
+              delay: 0.4,
+              opacity: { duration: 0.2 },
+            }}
+            className={styles.imageContainer}
+          >
             <Image
               width={1000}
               height={1000}
               alt="image of me"
-              src="https://images.unsplash.com/photo-1473830394358-91588751b241?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+              src="https://images.unsplash.com/photo-1676968986443-7f47aad7d993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80"
               className={styles.image}
             ></Image>
-          </div>
+          </motion.div>
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{
+              delay: 0.8,
+              opacity: { duration: 0.2 },
+            }}
+            className={styles.dataItemContainer}
+          >
+            <motion.p className={styles.dataItem}>{author}</motion.p>
+            <motion.p className={styles.dataItem}>{lastUpdated}</motion.p>
+            <motion.p className={styles.dataItem}>{timeToRead}</motion.p>
+          </motion.div>
         </div>
         <div className={styles.rightContentContainer}>
           <motion.div
@@ -38,7 +67,7 @@ export default function Blog() {
             animate={{ opacity: 1 }}
             initial={{ opacity: 0 }}
             transition={{
-              delay: 0.5,
+              delay: 1.5,
               opacity: { duration: 0.2 },
             }}
           >
