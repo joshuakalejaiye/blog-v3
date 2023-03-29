f681266d0ad025272f872c88297b31cf86ec9219
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2023-03-29 12:28:07 +0100

    loading tweaks

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -5,17 +5,29 @@ import { useReducedMotion } from "framer-motion";
 import Image from "next/image";
 import heroImage from "../../assets/ebun_oluwole_max.webp";
 import { useEffect, useState } from "react";
+import useScreenSize from "../../hooks/useScreenSize";
 
 const Hero = () => {
   const shouldReduceMotion = useReducedMotion();
   const [imageLoaded, setImageLoaded] = useState(true);
+  const [shouldMount, setShouldMount] = useState(false);
+
+  const { mobile } = useScreenSize()
 
   useEffect(() => {
     console.log(imageLoaded);
+    if (imageLoaded) {
+      setShouldMount(true)
+    }
   }, [imageLoaded]);
 
+  //this is extremely stupid lol
+  const extraProps = {
+    unoptimized: mobile
+  }
+
   return (
-    <div className={styles.container}>
+    shouldMount ? <div className={styles.container}>
       <div draggable="false" className={styles.textSection}>
         <motion.div
           animate={imageLoaded && { opacity: 1 }}
@@ -33,6 +45,7 @@ const Hero = () => {
             src={heroImage}
             fill
             alt="Image of a bustling city (Nottingham) in the daytime"
+            {...extraProps}
           ></Image>
         </motion.div>
         <motion.div
@@ -68,7 +81,7 @@ const Hero = () => {
           </p>
         </motion.div>
       </div>
-    </div>
+    </div> : <div className={styles.loading}><p style={{ color: 'white', fontSize: '30px'}}>loading... this will look better someday i promise</p></div>
   );
 };
 
